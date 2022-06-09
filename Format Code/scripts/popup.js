7cb3a82d22f081e98b1dc2afdbe7aab26a74fd33
kromate <akpananthony33@gmail.com>
2022-06-09 16:16:08 +0100

    feat adding tabs

---
+++ Format Code/scripts/popup.js
@@ -0,0 +1,20 @@
+let tabsContainer = document.querySelector("#tabs");
+
+let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
+
+tabTogglers.forEach(function(toggler) {
+  toggler.addEventListener("click", function(e) {
+    e.preventDefault();
+    let tabName = this.getAttribute("href");
+    let tabContents = document.querySelector("#tab-contents");
+    for (let i = 0; i < tabContents.children.length; i++) {
+      tabTogglers[i].parentElement.classList.remove("border-b", "border-purple-700");  tabContents.children[i].classList.remove("hidden");
+      if ("#" + tabContents.children[i].id === tabName) {
+        continue;
+      }
+      tabContents.children[i].classList.add("hidden");
+      
+    }
+    e.target.parentElement.classList.add("border-b", "border-purple-700");
+  });
+});
